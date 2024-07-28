bac4c256818088481e55f41c5b1f47e43b53ddc7
Artemy Egorov <artegoser@gmail.com>
2024-07-28 22:01:17 +0300

    fix(ts): exports

--- libs/typescript/src/lib.ts
+++ libs/typescript/src/lib.ts
@@ -1,5 +1,1 @@
-import El from "./daletl/tags/el";
-import Heading from "./daletl/tags/heading";
-
-export { parse, RootClass } from "./daletl/main";
-export { El, Heading };
+export * from "./daletl/types";
